dfececa8b41b28cc4609419d97926511cc9d25c1
singhabhi03 <abhishek.nsec04@gmail.com>
2024-10-04 18:22:57 -0400

    Cart commit two

--- src/main/java/com/tmb/pages/CartCherryPick.java
+++ src/main/java/com/tmb/pages/CartCherryPick.java
@@ -5,5 +5,7 @@ public class CartCherryPick {
 	public static void main(String[] args) {
 		System.out.println("Cart Branch First Commit");
 	}
-	
+	public void test() {
+		System.out.println("second commit");
+	}
 }
